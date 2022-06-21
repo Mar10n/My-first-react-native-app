f48a74c79f96a0bc33556e7fb8ce3f3ec4bd097e
Martin <ultra1337dk@gmail.com>
2022-06-21 09:55:03 +0200

    Initiated expo project

---
+++ News-Application/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="All" component={All} />
+        <Tab.Screen name="Business" component={Business} />
+        <Tab.Screen name="Health" component={HealthScreen} />
+        <Tab.Screen name="Sports" component={SportsScreen} />
+        <Tab.Screen name="Tech" component={TechScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
